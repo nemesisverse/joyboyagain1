006a0c8333cd52bde6b774e058cf13a0d468be98
nemesisverse <129728622+nemesisverse@users.noreply.github.com>
2024-07-21 09:05:21 +0530

    Enemy killing

--- Assets/EnemyAttack.cs
+++ Assets/EnemyAttack.cs
@@ -18,7 +18,7 @@ public class EnemyAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        AttackHitEvent();
+      
     }
 
 
--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -14,15 +14,5 @@ public class PlayerHealth : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
 }
