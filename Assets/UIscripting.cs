427e28a47dae31355088cb11fb9888b9456a46d4
nemesisverse <129728622+nemesisverse@users.noreply.github.com>
2024-07-21 14:46:13 +0530

    p

--- Assets/UIscripting.cs
+++ Assets/UIscripting.cs
@@ -1,17 +1,35 @@
+using Cinemachine;
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class UIscripting : MonoBehaviour
 {
-    public GameObject cam25;
-    public GameObject cam3;
+    public CinemachineFreeLook cam25
+        ;
+    public CinemachineFreeLook cam3;
+
+    private CinemachineFreeLook activeCamera;
+
     [SerializeField] Canvas gameOverCanvas;
+    public Camera main;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(Camera.main != null)
+        {
+            main.enabled = false;
+            main.enabled = true;
+
+        }
+
+        // Initially set camera1 as the active camera
+        activeCamera = cam25;
+        cam25.Priority = 3;
+        cam3.Priority = 2;
+
     }
 
     // Update is called once per frame
@@ -22,7 +40,7 @@ public class UIscripting : MonoBehaviour
 
     public void Try()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
     public void GoBack()
     {
@@ -31,16 +49,23 @@ public class UIscripting : MonoBehaviour
 
     public void SetCam25()
     {
-       cam3.SetActive(false);
-        cam25.SetActive(true);
+        cam25.Priority = 1;
+        cam3.Priority = 0;
+        cam3.enabled = false;
         gameOverCanvas.enabled = false;
     }
 
     public void SetCam3()
     {
 
-        cam25.SetActive(false);
-        cam3.SetActive(true);
+        cam25.Priority = 0;
+        cam3.Priority = 1;
+        cam25.enabled = false;  
         gameOverCanvas.enabled = false;
+    }
+
+    public void quit()
+    {
+        Application.Quit();
     }
 }
